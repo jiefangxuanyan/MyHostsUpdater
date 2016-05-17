00f1f820654f463554a822c46e97b2105252f802
jiefangxuanyan <505745416@qq.com>
2016-05-17 13:54:27 +0800

    没删干净~~

--- methods.py
+++
@@ -1,7 +0,0 @@
-# coding=utf-8
-import urllib2
-
-methods = {
-    None: urllib2.build_opener(),
-    u"XX-Net": urllib2.build_opener(urllib2.ProxyHandler({'http': 'localhost:8087', 'https': 'localhost:8087'})),
-}
