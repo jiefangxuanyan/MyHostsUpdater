0fe6b57755fb418abbff782f85923e84e8ea15ba
jiefangxuanyan <505745416@qq.com>
2016-11-09 17:48:04 +0800

    我觉得我该更新下样例配置~~

--- example/config.py
+++ example/config.py
@@ -16,12 +16,13 @@ defaults = [
 ]
 
 sources = [
+    (u"lennylxx", u"https://raw.githubusercontent.com/lennylxx/ipv6-hosts/master/hosts", None),
     (u"laod", u"file:///G:/software/Windows系列跟苹果系列/hosts", None),
     (u"racaljk", u"https://raw.githubusercontent.com/racaljk/hosts/master/hosts", None),
     (u"txthinking", u"https://raw.githubusercontent.com/txthinking/google-hosts/master/hosts", None),
-    (u"yadgen", u"http://blog.yadgen.com/wp-content/uploads/2013/01/hosts_69.2.txt", None),
+    (u"yadgen", u"http://blog.yadgen.com/wp-content/uploads/2013/01/hosts_71.txt", None),
 ]
 
 excludes = [
-    re.compile(ur"github\.com$"),
+    re.compile(ur"(?<!gist\.)github\.com$"),
 ]
